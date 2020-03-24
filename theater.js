00a60d12636c58dc06b66c43c92eba9af0d8cb29
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2020-03-24 12:19:00 +0100

    extract format

--- theater.js
+++ theater.js
@@ -34,13 +34,16 @@ let volumeCreditsFor = function (aPerformance) {
     return volumeCredits;
 };
 
+let format = function(aNumber) {
+    return new Intl.NumberFormat("en-US",
+    {style: "currency", currency: "USD",
+        minimumFractionDigits: 2 }).format(aNumber);
+};
+
 exports.statement = function(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
         // print line for this order
