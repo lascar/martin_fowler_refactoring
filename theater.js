12e7989869b68bb55c6d667a4081f297476aa940
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2020-03-24 12:34:49 +0100

    extract totalVolumeCredits

--- theater.js
+++ theater.js
@@ -40,17 +40,23 @@ let usd = function(aNumber) {
         minimumFractionDigits: 2 }).format(aNumber/100);
 };
 
-exports.statement = function(invoice, plays) {
-    let totalAmount = 0;
+let totalVolumeCredits = function(invoice) {
     let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+};
+
+exports.statement = function(invoice, plays) {
+    let totalAmount = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    for (let perf of invoice.performances) {
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))}\n`;
         totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
     return result;
 };
