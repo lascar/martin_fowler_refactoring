c79ca3703302141cc26a9943c0b546b97969ca88
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2020-03-25 17:18:54 +0100

    add html render

--- test_theater.js
+++ test_theater.js
@@ -1,7 +1,9 @@
 var assert = require('assert');
 let invoices = require( './invoices.json');
 let theater = require ('./theater.js');
-let expected = `Statement for BigCo\n  Hamlet: $290.00 (55 seats)\n  As You Like It: $220.00 (35 seats)\n  Othello: $140.00 (40 seats)\nAmount owed is $650.00\nYou earned 47 credits\n`
+let expected = `Statement for BigCo\n  Hamlet: $290.00 (55 seats)\n  \
+As You Like It: $220.00 (35 seats)\n  Othello: $140.00 (40 seats)\n\
+Amount owed is $650.00\nYou earned 47 credits\n`
 let result = theater.statement(invoices[0]);
 try {
     assert.equal(expected, result);
@@ -9,5 +11,22 @@ try {
 } catch (error) {
     console.error('Failed');
     console.error(error);
-}
-
+};
+let expected_html = `<h1>Statement for BigCo</h1>\
+    <table>\
+    <tr><th>play</th><th>seats</th><th>cost</th></tr>\
+    <tr><td>Hamlet</td><td>55</td><td>$290.00</td></tr>\
+    <tr><td>As You Like It</td><td>35</td><td>$220.00</td></tr>\
+    <tr><td>Othello</td><td>40</td><td>$140.00</td></tr>\
+    </table>\
+    <p>Amount owed is <em>$650.00</em></p>\
+    <p>You earned <em>47</em> credits</p>`
+let result_html = theater.htmlStatement(invoices[0]);
+try {
+    assert.equal(expected_html.replace(/\s+/g, "").replace(/\/n/g, ""),
+        result_html.replace(/\s+/g, "").replace(/\/n/g, ""));
+    console.log('Passed');
+} catch (error) {
+    console.error('Failed');
+    console.error(error);
+};
--- theater.js
+++ theater.js
@@ -6,6 +6,20 @@ let usd = function(aNumber) {
         minimumFractionDigits: 2 }).format(aNumber/100);
 };
 
+let renderHtml = function (data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += `<table>\n`;
+    result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`;
+    for (let perf of data.performances) {
+        result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>/n`;
+    }
+    result += `</table>\n`;
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+};
+
 let renderPlainText = function(data) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
@@ -15,6 +29,10 @@ let renderPlainText = function(data) {
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
+};
+
+exports.htmlStatement = function(invoice) {
+    return renderHtml(createStatementData(invoice));
 };
 
 exports.statement = function(invoice) {
